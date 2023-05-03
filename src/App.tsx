1b302b7907936156656674f8f022fb0c91cd6e4c
Jekarter <jkartyalta@yandex.ru>
2023-05-03 13:58:57 +0500

    feat(pages): add page home and favourite. Add navigation panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation';
+import FavouritesPage from './pages/FavouritesPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="font-bold">
-      hello
-    </div>
+    <>
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/favourites' element={<FavouritesPage/>} />
+      </Routes>
+    </>
   );
 }
 
