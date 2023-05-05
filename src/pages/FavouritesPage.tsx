410add86a12632bbcd179b368ded47c4d28d2305
Jekarter <jkartyalta@yandex.ru>
2023-05-05 16:11:34 +0500

    feat(dropdown, data): add data, dropdown, card

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useAppSelector } from '../hooks/redux';
 
 const FavouritesPage = () => {
+  const {favourites}  = useAppSelector(state => state.github);
+
+  if (favourites.length === 0) return <p className='text-center'>No items.</p>
   return (
-    <div>FavouritesPage</div>
+    <ul className='list-none'>
+      {favourites.map(item => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import RepoCard from '../components/RepoCard'
 import useDebounce from '../hooks/debounce'
-import { useSearchUsersQuery } from '../store/github/github.api'
+import { useLazyGetUserReposQuery, useSearchUsersQuery } from '../store/github/github.api'
 
 const HomePage = () => {
   const [search, setSearch] = useState<string>('')
+  const [dropdown, setDropdown] = useState(false)
   const debounced = useDebounce(search);
-  const {isLoading, isError, data} = useSearchUsersQuery('jekarter')
+  const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+    skip: search.length < 3,
+  })
+  const [fetchRepos, {isLoading: areReposLoading, data: repo}] = useLazyGetUserReposQuery()
 
-  console.log(debounced)
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data])
+
+  const clickHandler = (username: string) => {
+    fetchRepos(username)
+    setDropdown(false)
+  }
 
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -19,8 +31,19 @@ const HomePage = () => {
           value={search}
           onChange={e => setSearch(e.target.value)}
         />
-        <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'> 
-          some text
+        {dropdown && <ul className='list-none overflow-y-scroll absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'> 
+        {isLoading && <p>Loading...</p>} 
+          {data?.map(user => 
+            <li 
+              key={user.id}
+              onClick={() => clickHandler(user.login)}
+              className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+            >{user.login}</li>
+          )}
+        </ul>}
+        <div className='containter'>
+          {areReposLoading && <p className='text-center'>Repos are loading</p>}
+          {repo?.map(rep => <RepoCard repo={rep} key={rep.id} />)}
         </div>
       </div>
     </div>
